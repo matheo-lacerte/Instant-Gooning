5b50881631afd1b2be3203fb9af973797c4c66b7
Matheo <mlacerte2013@gmail.com>
2025-09-18 15:32:09 -0400

    Ajout d'une helper function s'assurer que req.user.id existe

--- server/routes/dev.js
+++ server/routes/dev.js
@@ -1,14 +1,11 @@
 import express from 'express'
 import pool from "../config/db.js"
 import authMiddleware from '../middleware/auth.js'
-
+import requireUser from '../middleware/requireUser.js'
 const router = express.Router()
 
-router.post("/join", authMiddleware, async (req, res) => {
+router.post("/join", authMiddleware, requireUser, async (req, res) => {
     try {
-        if (!req.user || !req.user.id) {
-            return res.status(401).json({ error: "Non autorisé" });
-        }
         await pool.query("UPDATE users SET is_a_developer = true WHERE id = $1", [req.user.id]);
         return res.json({ message: "Vous êtes maintenant un développeur !" });
     } catch (err) {
@@ -16,11 +13,8 @@ router.post("/join", authMiddleware, async (req, res) => {
     }
 });
 
-router.post("/leave", authMiddleware, async (req, res) => {
+router.post("/leave", authMiddleware, requireUser, async (req, res) => {
     try {
-        if (!req.user || !req.user.id) {
-            return res.status(401).json({ error: "Non autorisé" });
-        }
         await pool.query("UPDATE users SET is_a_developer = false WHERE id = $1", [req.user.id]);
         return res.json({ message: "Vous n'êtes plus un développeur." });
     } catch (err) {
