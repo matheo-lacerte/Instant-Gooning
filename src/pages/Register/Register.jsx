924fa46fa7913c4b44fd23326a1513ae7403909c
AnasRassiane <2384229@cmontmorency.qc.ca>
2025-09-24 14:35:50 -0400

    Ajout d'une connexion vers Supabase lors d'une inscription

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -3,21 +3,48 @@ import { useNavigate, Link } from "react-router-dom";
 export default function Login() {
   const navigate = useNavigate();
 
-  const authSubmitHandler = () => {
-    navigate("/login");
+  const registerSubmitHandler = async (event) => {
+    event.preventDefault();
+
+    const fd = new FormData(event.target);
+    const data = Object.fromEntries(fd.entries());
+    const username = data.username.trim();
+    const password = data.password.trim();
+    const email = data.email.trim();
+    const first_name = data.first_name.trim();
+    const last_name = data.last_name.trim();
+
+    try {
+      const response = await fetch("http://localhost:5174/api/auth/register", {
+        method: "POST",
+        headers: {  "Content-Type": "application/json" },
+        body: JSON.stringify({ username, email, password, first_name, last_name }),
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        alert(errorData.error);
+        throw new Error("Données invalides");
+      }
+
+      alert("Ne reste plus qu'à vous connecter!");
+      navigate("/login");
+    } catch (error) {
+      console.error("Erreur lors de l'inscription :", error);
+    }
   };
 
   return (
-    <form onSubmit={authSubmitHandler}>
+    <form onSubmit={registerSubmitHandler}>
       <h1>Inscription</h1>
 
       <div className="control-row">
         <div className="control no-margin">
-          <label htmlFor="username">Courriel</label>
+          <label htmlFor="email">Courriel</label>
           <input
-            id="username"
+            id="email"
             type="text"
-            name="username"
+            name="email"
             placeholder="Entrez votre courriel"
             required
           />
@@ -29,6 +56,28 @@ export default function Login() {
             id="username"
             type="text"
             name="username"
+            placeholder="Entrez votre nom d'utilisateur"
+            required
+          />
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="username">Prénom</label>
+          <input
+            id="first_name"
+            type="text"
+            name="first_name"
+            placeholder="Entrez votre prénom"
+            required
+          />
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="username">Nom</label>
+          <input
+            id="last_name"
+            type="text"
+            name="last_name"
             placeholder="Entrez votre nom"
             required
           />
