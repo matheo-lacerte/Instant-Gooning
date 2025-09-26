616715cae00488888e33ccbfb0c19eaf947e0b8e
AnasRassiane <2384229@cmontmorency.qc.ca>
2025-09-26 13:39:34 -0400

    Ajout du contenu css pour la page Logout

--- src/pages/Logout/Logout.jsx
+++ src/pages/Logout/Logout.jsx
@@ -2,34 +2,40 @@ import { Link, useNavigate } from "react-router-dom";
 import AuthContext from "../../app/Context/AuthContext.jsx";
 import { useContext } from "react";
 
+import "./logout.css";
+
 const Logout = () => {
   const auth = useContext(AuthContext);
   const navigate = useNavigate();
 
   const deconnexion = async () => {
-      const reponse = await fetch("http://localhost:5174/api/auth/logout", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-      });
+    const reponse = await fetch("http://localhost:5174/api/auth/logout", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    });
 
-      if (!reponse.ok) {
-        throw new Error("Erreur lors de la déconnexion");
-      }
+    if (!reponse.ok) {
+      throw new Error("Erreur lors de la déconnexion");
+    }
 
-      const reponseData = await reponse.json();
-      alert(reponseData.message);
-      auth.logout();
-      navigate("/");
+    const reponseData = await reponse.json();
+    alert(reponseData.message);
+    auth.logout();
+    navigate("/");
   };
 
   return (
-    <>
+    <div className="logout-page">
       <h1>Voulez-vous déconnecter?</h1>
-      <button onClick={deconnexion}>Oui</button>
-      <button>
-        <Link to="/dev">Non</Link>
-      </button>
-    </>
+      <div className="logout-choices">
+        <button className="button_logout green" onClick={deconnexion}>
+          Oui
+        </button>
+        <button className="button_logout red">
+          <Link to="/dev">Non</Link>
+        </button>
+      </div>
+    </div>
   );
 };
 
