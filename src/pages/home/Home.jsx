51c18d5995aabf5ae0e936ebd764059fec9e02ba
AnasRassiane <2384229@cmontmorency.qc.ca>
2025-10-17 15:44:44 -0400

    Modification du code pour afficher les jeux sauvegardés dans la base de données

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,31 @@
-import { mockGames } from "../../app/data/mockGames";
 import GameCard from "../../app/components/GameCard";
+import { useState } from "react";
 import "./home.css";
 
 export default function Home() {
+  const [listeJeux, setListeJeux] = useState ([]);
+  useState (async () => {
+    try {
+      const response = await fetch(
+        "http://localhost:5174/api/games/GetAllGames",
+        {
+          method: "GET",
+        }
+      );
+
+      const reponseData = await response.json();
+      setListeJeux(reponseData);
+    } catch (err) {
+      alert("Une erreur est survenue. Veuillez réessayer plus tard.");
+    }
+  });
+
   return (
     <div className="home-wrap">
       <h1 className="home-title">Jeux populaires</h1>
       <div className="games-grid ig-grid">
-        {mockGames.map((g) => (
-          <GameCard key={g.id} game={g} />
+        {listeJeux.map((j) => (
+          <GameCard key={j.id} game={j} />
         ))}
       </div>
     </div>
