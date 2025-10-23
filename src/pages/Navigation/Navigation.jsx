d73436a6dd137731af1d9b1947ca2e3ebfdfda28
AnasRassiane <2384229@cmontmorency.qc.ca>
2025-10-23 12:46:53 -0400

    Ajout d'une barre de recherche afin de chercher des jeux

--- src/pages/Navigation/Navigation.jsx
+++ src/pages/Navigation/Navigation.jsx
@@ -1,14 +1,26 @@
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../app/Context/AuthContext";
+import { useNavigate } from "react-router-dom";
 import "./navigation.css";
 
 const Navigation = () => {
   const auth = useContext(AuthContext);
+  const navigate = useNavigate();
+  const recherche = () => {
+    if (!auth.search || auth.search.trim() === "") return;
+    navigate(`/search?search=${encodeURIComponent(auth.search.trim())}`);
+  };
 
   return (
     <header className="main-header">
-      <Link to="/" className="title">Instant Gooning</Link>
+      <Link to="/" className="title"> Instant Gooning </Link>
+      <div className="search-bar">
+        <input type="text" placeholder="Rechercher un jeu..." value={auth.search} onChange={(e) => auth.setSearch(e.target.value)} />
+        <button onClick={recherche}>
+          <img src="/loupe.png" className="image" />
+        </button>
+      </div>
       <div className="nav-buttons">
         {!auth.isLoggedIn ? (
           <>
@@ -18,12 +30,11 @@ const Navigation = () => {
             <Link to="/register">
               <button>Inscription</button>
             </Link>
+
           </>
         ) : (
           <Link to="/dev">
-            <button id="droite">
-              Profil
-            </button>
+            <button id="droite">Profil</button>
           </Link>
         )}
       </div>
