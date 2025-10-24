33447f1cc3c8785b63df36c072d63f2505b1f5be
Matheo <mlacerte2013@gmail.com>
2025-10-24 17:04:31 -0400

    Ajout de la fonctionnalité de refus de demande et mise à jour de l'acceptation pour vérifier l'état de la demande

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,10 +1,11 @@
 import express from "express";
 import authMiddleware from "../middleware/auth.js";
 import requireUser from "../middleware/requireUser.js";
-import {postForm, getAllRequests, acceptRequest} from "../controllers/adminController.js"
+import {postForm, getAllRequests, acceptRequest, declineRequest} from "../controllers/adminController.js"
 const router = express.Router();
 
 router.post("/request-dev", authMiddleware, requireUser, postForm);
 router.get("/getAllRequests", authMiddleware, requireUser, getAllRequests);
 router.post("/acceptRequest", authMiddleware, requireUser, acceptRequest);
+router.post("/declineRequest", authMiddleware, requireUser, declineRequest);
 export default router;
