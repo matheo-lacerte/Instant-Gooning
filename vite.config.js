3227fe7da2c359e1702e17027a0ef2f2422977e9
Matheo <mlacerte2013@gmail.com>
2025-12-05 16:42:47 -0500

    Ajout de test unitaire avec vitest

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,19 @@ export default defineConfig({
     proxy: {
       '/api': 'http://localhost:5174'
     }
+  },
+  test: {
+    environment: 'jsdom',
+    setupFiles: './src/test/setupTests.js',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+      lines: 75,
+      functions: 75,
+      branches: 75,
+      statements: 75,
+      include: ['src/app/components/**/*.{js,jsx}', 'src/app/Context/**/*.{js,jsx}'],
+      exclude: ['src/**/*.css', 'src/pages/**', 'server/**', 'cypress/**']
+    }
   }
 })
